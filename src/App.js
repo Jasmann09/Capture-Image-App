77dcd84d8f7f98546eeac09209bd3249af48c566
Jasmann09 <js0289@srmist.edu.in>
2023-03-05 14:25:46 +0530

    Capture : React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,72 @@
+// eslint-disable-next-line no-unused-vars
 import logo from './logo.svg';
 import './App.css';
 
+import React, { useEffect, useRef } from "react";
+
 function App() {
+  let videoRef = useRef(null);
+
+  let photoRef = useRef(null)
+
+  const getVideo = () => {
+    navigator.mediaDevices
+      .getUserMedia({
+        video: true
+      })
+      .then((stream) => {
+        let video = videoRef.current;
+        video.srcObject = stream;
+        video.play();
+      })
+      .catch((err) => {
+        console.error(err);
+      });
+  };
+
+  const takePicture = () => {
+    const width = 400
+    const height = width / (16 / 9)
+    
+    let video = videoRef.current
+
+    let photo = photoRef.current
+
+    photo.width = width
+
+    photo.height = height
+
+    let ctx = photo.getContext('2d')
+
+    ctx.drawImage(video, 0, 0, width, height)
+    
+  }
+
+  const clearImage = () => {
+    let photo = photoRef.current
+
+    let ctx = photo.getContext('2d')
+
+    ctx.clearRect(0,0,photo.width,photo.height)
+  }
+
+  useEffect(() => {
+    getVideo();
+  }, [videoRef]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 className="text-center"><center>Camera Selfie App in React</center></h1>
+
+      <video ref={videoRef} className="container"></video>
+      {/* <br></br> */}
+      <button onClick={takePicture} className="btn-1">Take Picture</button>
+
+      <canvas className="container" ref={photoRef}></canvas>
+
+      <button onClick={clearImage} className="btn-2">Clear Image</button>
+
+      <br/><br/>
     </div>
   );
 }
